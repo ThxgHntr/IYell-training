9a38d55fbf46e990ab8f990fceeff9eb8a66c37b
trinhdamhuy@gmail.com <trinhdamhuy@gmail.com>
2023-07-19 12:01:25 +0700

    grammar fixed

--- 同じ機能-Laravel/resources/views/employee/succeed.blade.php
+++ 同じ機能-Laravel/resources/views/employee/succeed.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-slot:title>
-        削除
+        完了
     </x-slot:title>
     <div class="d-flex justify-content-center align-items-center mx-5 px-5" style="height: 70%;">
         <span class="border border-3 fw-bold px-5 pt-5">完了しました</br>
